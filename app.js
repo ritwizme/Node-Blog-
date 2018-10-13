952e233fe44056f4e32c9cf2037f36a8427e6a4f
Ritwiz <ritwizme@gmail.com>
2018-10-14 00:07:44 +0530

    'regs_complete'

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
@@ -43,6 +44,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json());
 
+//Static folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 //Method Override middleware
 app.use(methodOverride('_method'));
 
@@ -62,7 +68,7 @@ app.use(session({
 
 app.use(function(req, res, next){
     res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg_msg = req.flash('rerror_msg');
+    res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
     next();
 });
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,88 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const router = express.Router();
-module.exports = router;
+
+//Load user model
+require('../models/User');
+const User = mongoose.model('users');
 
 
 //User Login route
 router.get('/login', (req, res) => {
-    res.send('Login');
+    res.render('users/login');
 });
 
 //User Register route
 router.get('/register', (req, res) => {
-    res.send('Register');
+    res.render('users/register');
 });
 
+//Reegister form POST
+router.post('/register', (req, res) =>{
+ let errors = [];
+ if (req.body.password != req.body.password2){
+     errors.push({text: 'Passwords donot match'});
+ }
+ if(req.body.password.length < 4){
+     errors.push({text: 'Password must be atleast 4 characters'});
+ }
+if(errors.length > 0){
+    res.render('users/register', {
+        errors: errors,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        password2: req.body.password2
+    });
+}
+else{
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if(user){
+            req.flash('error_msg', 'Email already registered');
+            res.redirect('/users/register');
+        }
+        else{
+
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+        
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) =>{
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user =>{
+                        req.flash('success_msg', 'You are now registered and can login');
+                        res.redirect('/users/login');
+                    })
+                    .catch(err =>{
+                        console.log(err);
+                        return;
+                    });
+                });
+            });
+        }
+
+
+    });
+
+
+    //console.log(newUser);
+
+}
+
+});
+
+
+
+module.exports = router;
 
 
 
