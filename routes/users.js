fc20ba0fe9e8e859f57240836277e2ddf6c188e3
Ritwiz <ritwizme@gmail.com>
2018-10-15 17:38:58 +0530

    'logout_functionality'

--- routes/users.js
+++ routes/users.js
@@ -89,7 +89,13 @@ else{
 
 });
 
+//Logout route
 
+router.get('/logout',(req, res)=>{
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
 
 module.exports = router;
 
